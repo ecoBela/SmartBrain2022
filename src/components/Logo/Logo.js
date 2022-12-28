1bbe98df3fa5152424d8e5a18934850845526ec0
Bela Hamid <belahamid@gmail.com>
2022-12-28 01:29:17 +0000

    Add icon image to logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Tilt from "react-parallax-tilt";
 import "./Logo.css";
+import brain from "./brain.png";
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
-      <Tilt>
+      <Tilt tiltMaxAngleX={85} perspective={2000} tiltAxis={"x"}>
         <div
-          className=" Tilt br2 shadow-2"
+          className=" Tilt br2 shadow-2 pa3"
           style={{
             width: "150px",
             height: "150px",
           }}
         >
-          <h1>React Parallax Tilt 👀</h1>
+          <img src={brain} alt="brain icon" style={{ paddingTop: "5px" }}></img>
         </div>
       </Tilt>
     </div>
