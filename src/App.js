9c33f998cd60eaaf4d628bd840685a011e21e531
Bela Hamid <belahamid@gmail.com>
2022-12-28 22:38:07 +0000

    Create functions for image link form component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
@@ -6,13 +7,21 @@ import Rank from "./components/Rank/Rank";
 import ParticlesBg from "particles-bg";
 
 function App() {
+  const [input, setInput] = useState("");
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+  const onSubmit = () => {
+    console.log("click");
+  };
+
   return (
     <div className="App">
       <ParticlesBg className="particles" type="lines" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={onInputChange} onSubmit={onSubmit} />
 
       {/* <FaceRecognition /> */}
     </div>
