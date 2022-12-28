0ab4029f386859b52a3ed6dadadb118f6220a1c5
Bela Hamid <belahamid@gmail.com>
2022-12-28 01:05:32 +0000

    Add logo and import packages

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
+import "./Logo.css";
 
 const Logo = () => {
-  return <div className="ma4 mt0"></div>;
+  return (
+    <div className="ma4 mt0">
+      <Tilt>
+        <div
+          className=" Tilt br2 shadow-2"
+          style={{
+            width: "150px",
+            height: "150px",
+          }}
+        >
+          <h1>React Parallax Tilt 👀</h1>
+        </div>
+      </Tilt>
+    </div>
+  );
 };
 
 export default Logo;
