e2910ab78e4f5a739e0547a3a6cb73aec85076fd
Bela Hamid <belahamid@gmail.com>
2023-01-06 23:15:18 +0000

    Troubleshooting type error on axios call

--- src/App.js
+++ src/App.js
@@ -12,18 +12,21 @@ const app = new Clarifai.App({
   apiKey: "f31deb97a0664700931d3a747bda9189",
 });
 
+console.log(Clarifai);
+
 function App() {
   const [input, setInput] = useState("");
   const [imageUrl, setImageUrl] = useState("");
+  // const [box, setBox] = useState({});
 
-  const calculateFaceLocation = (data) => {
-    const ClarifaiFace =
-      data.outputs[0].data.regions[0].region_info_bounding_box;
-    const image = document.getElementById("inputImage");
-    const width = Number(image.width);
-    const height = Number(image.height);
-    console.log(width, height);
-  };
+  // const calculateFaceLocation = (data) => {
+  //   const ClarifaiFace =
+  //     data.outputs[0].data.regions[0].region_info_bounding_box;
+  //   const image = document.getElementById("inputImage");
+  //   const width = Number(image.width);
+  //   const height = Number(image.height);
+  //   console.log(width, height);
+  // };
 
   const onInputChange = (event) => {
     setInput(event.target.value);
@@ -33,9 +36,11 @@ function App() {
     setImageUrl(input);
     console.log("howdy: before");
     app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, input)
-      .then((response) => calculateFaceLocation(response))
+      .predict("a403429f2ddf4b49b307e318f00e528b", input)
+      .then((response) => console.log(response))
       .catch((err) => console.log(err));
+
+    console.log("howdy: after");
   };
 
   return (
