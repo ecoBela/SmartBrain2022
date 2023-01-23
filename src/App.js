ec311eb7ef8ff62c2d682026b59e7818f6663540
Bela Hamid <belahamid@gmail.com>
2023-01-23 18:43:09 +0000

    Start connecting FE to BE

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
@@ -67,6 +67,12 @@ function App() {
     }
     setRoute(route);
   };
+
+  useEffect(() => {
+    fetch("http://localhost:3001/")
+      .then((response) => response.json())
+      .then(console.log);
+  }, []);
 
   return (
     <div className="App">
